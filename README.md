b1d86ca451f392e36859dca6ae978ebf1b7cb56f
Ryan Siu <ryansiu@uw.edu>
2017-10-13 15:20:06 +0800

    First commit with part of server-side scripts

---
+++ README.md
@@ -0,0 +1,29 @@
+Python Shell in NodeJS
+====================================================
+A light-weight application to invoke python
+program in nodeJS. This application is developed in
+my workplace for faciliating the work of colleagues 
+who cannot code. The source codes are now under 
+reorganization.
+
+Purpose of this project
+----------------------------------------------------
+1. Execute a python program without ssh
+2. Sending the stdin and stdout from python to
+user for interaction
+3. Multi-background python shell (Not implemented yet)
+4. Logging the output from python (Not implemented yet)
+
+Warning
+----------------------------------------------------
+The source has not yet reorganized after launching
+in my work place. The functionality may not be completed
+or bug-free. Welcome anyone who are interested in 
+nodeJS to contribute.
+
+License
+----------------------------------------------------
+This plugin is licensed under the MIT license: 
+http://en.wikipedia.org/wiki/MIT_License
+
+Copyright © 2017 by Ryan Siu
---
+++ index.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var spawn = require('child_process').spawn;
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var fs = require('fs');
+
+var uint8arrayToString = function(data){
+	return String.fromCharCode.apply(null, data);
+};
+
+io.sockets.on('connection', function(socket) {
+	// Send any error to client
+	socket.on('error', function (data) {
+		console.log(data || 'error');
+	});
+
+	// Kill the process
+	socket.on('python/kill', function(data) {
+		shell.kill('SIGINT');
+	})
+
+	// Execute the python programs
+	socket.on('python/execute', function(data, callback) {
+		if (!pythonPending) {
+			shell = spawn('python', ['-u', 'python/' + data.name]);
+			
+			callback({msg: 'success', state: true});
+			
+			pending = true;
+
+			shell.stdout.on('data', (msg) => {
+				msg = uint8arrayToString(msg);	
+				socket.emit('python/msg', {msg: msg});
+			})
+
+			shell.stderr.on('data', (msg) => {
+				msg = uint8arrayToString(msg);
+				socket.emit('python/msg', {msg: msg})
+			})
+
+			shell.on('exit', (code) => {
+				socket.emit('python/msg', {msg: 'Program ended with code ' + code + '<br>', ended: true})
+				pythonPending = false;
+			})
+
+			socket.on('python/arg', function(data) {
+				shell.stdin.write(data + '\n');
+			})
+
+			pythonPending = true;
+		} else {
+			callback({msg: 'There is a program running in server. If you want to kill it, please tell the maintainer<br>', state: false})
+		}
+	})
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Start the server
+server.listen(413, function() {
+	console.log('Server in port 413 is ready');
+});
